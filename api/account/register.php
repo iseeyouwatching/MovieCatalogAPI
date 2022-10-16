5a15ed74d6e0c88207c26fcf3023c318a0daadd3
iseeyouwatching <leonidshibanovv@gmail.com>
2022-10-16 22:34:04 +0700

    Made a bad version of the registration request

---
+++ api/account/register.php
@@ -0,0 +1,31 @@
+<?php
+
+	require_once "database_connection.php";
+
+	function route($method, $urlList, $requestData) {
+		if ($method == "POST") {
+			global $link;
+			$username = $requestData->body->username;
+			$user = $link->query("SELECT user_id FROM users WHERE username='$username'")->fetch_assoc();
+			if (!$user) {
+				$name = $requestData->body->name;
+				$password = hash("sha1", $requestData->body->password);
+				$email = $requestData->body->email;
+				$gender = $requestData->body->gender;
+				$userInsertResult = $link->query("INSERT INTO users(username, name, password, email, gender) 
+												VALUES('$username', '$name', '$password', '$email', '$gender')");
+				if (!$userInsertResult) {
+					echo "too bad";
+				}
+				else {
+					echo "success";
+				}
+			}
+			else {
+				echo "user exist";
+			}
+		}
+		else {
+			"404";
+		}
+	}
--- index.php
+++ index.php
@@ -1,1 +1,27 @@
 <?php
+
+	global $link;
+
+	require_once "database_connection.php";
+	require_once "get_functions.php";
+
+	header("Content-type: application/json");
+
+	$url = isset($_GET['q']) ? $_GET['q'] : '';
+	$url = rtrim($url, '/');
+	$urlList = explode('/', $url);
+
+	$router = $urlList[2];
+	$requestData = getData(getMethod());
+	$method = getMethod();
+
+
+	if (file_exists(realpath(dirname(__FILE__)). '/' . $urlList[0] . '/'. $urlList[1] . '/'. $router . '.php')) {
+		include_once $urlList[0] . '/'. $urlList[1] . '/'. $router . '.php';
+		route($method, $urlList, $requestData);
+	}
+	else {
+		echo "NOPE 404";
+	}
+
+
