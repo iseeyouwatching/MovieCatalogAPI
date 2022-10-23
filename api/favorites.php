f8af47bebb5f4ce14a7bd7af53e88b8079e22a50
iseeyouwatching <leonidshibanovv@gmail.com>
2022-10-23 19:54:01 +0700

    Made processing a request to add a movie to the list of favorites

--- api/favorites.php
+++ api/favorites.php
@@ -4,4 +4,22 @@
 
 	function route($method, $urlList, $requestData) {
 		global $link;
+		switch ($method) {
+			case "POST":
+				$token = substr(getallheaders()['Authorization'], 7);
+				$userFromToken = $link->query("SELECT user_id FROM tokens WHERE value='$token'")->fetch_assoc();
+				$userId = $userFromToken['user_id'];
+				$movieId = $urlList[2];
+				$insertMovie = $link->query("INSERT INTO favourite_movies(user_id, movie_id) VALUES('$userId', '$movieId')");
+				if (!$insertMovie) {
+					echo "already added";
+				}
+				else {
+					echo "200: success";
+				}
+				break;
+			default:
+				echo "404";
+				break;
+		}
 	}
