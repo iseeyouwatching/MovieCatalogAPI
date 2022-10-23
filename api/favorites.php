b9adc316744c6df693d6ebfa58f5143fb6ef5581
iseeyouwatching <leonidshibanovv@gmail.com>
2022-10-23 20:02:48 +0700

    Made processing a request to delete a movie from the list of favorite movies

--- api/favorites.php
+++ api/favorites.php
@@ -4,4 +4,19 @@
 
 	function route($method, $urlList, $requestData) {
 		global $link;
+		switch ($method) {
+			case "DELETE":
+				$movieId = $urlList[2];
+				$deleteMovieFromFavourites = $link->query("DELETE FROM favourite_movies WHERE movie_id='$movieId'");
+				if ($deleteMovieFromFavourites) {
+					echo "200: success";
+				}
+				else {
+					echo "bad";
+				}
+				break;
+			default:
+				echo "404";
+				break;
+		}
 	}
