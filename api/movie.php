618e2f0e60102953e8bb4a3c84faccb935c70d2b
iseeyouwatching <leonidshibanovv@gmail.com>
2022-10-25 22:33:09 +0700

    Did the processing of the request to add a review
    With this request we will add the following data to the database (review id, user id, movie id, review text, rating, anonymous review or not, datetime of creation)

--- api/movie.php
+++ api/movie.php
@@ -4,5 +4,34 @@
 
 	function route($method, $urlList, $requestData) {
 		global $link;
-
+		switch ($method) {
+			case "POST":
+				$token = substr(getallheaders()['Authorization'], 7);
+				$userFromToken = $link->query("SELECT user_id FROM tokens WHERE value='$token'")->fetch_assoc();
+				$userID = $userFromToken['user_id'];
+				$review = $link->query("SELECT review_id FROM reviews WHERE user_id='$userID'")->fetch_assoc();
+				if (!$review) {
+					$reviewText = $requestData->body->reviewText;
+					$rating = $requestData->body->rating;
+					$isAnonymous = $requestData->body->rating;
+					$movieID = $urlList[2];
+					$now = DateTime::createFromFormat('U.u', number_format(microtime(true), 6, '.', ''));
+					$nowFormatted = substr($now->format('Y-m-d\TH:i:s.u'), 0, -3) . 'Z';
+					$reviewInsertResult = $link->query("INSERT INTO reviews(review_id, user_id, movie_id, review_text, rating, is_anonymous, create_datetime) 
+														VALUES(UUID(), '$userID', '$movieID', '$reviewText', '$rating', '$isAnonymous', '$nowFormatted')");
+					if (!$reviewInsertResult) {
+						echo "too bad";
+					}
+					else {
+						echo "success";
+					}
+				}
+				else {
+					echo "review already exist";
+				}
+				break;
+			default:
+				echo "404";
+				break;
+		}
 	}
